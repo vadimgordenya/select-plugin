aaaf698b6d102484593c0c2533f336402c837b70
Vadzim_Hardzenia <vadimgordenya@gmail.com>
2020-08-15 11:50:30 +0300

    Implemented js part to Select plugin.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import { Select } from './select/select';
 import "./select/style.scss";
 
-const select = new Select();
+const select = new Select("#select", {
+    placeholder: 'Выберите элемент',
+    selectedId: '3',
+    data: [
+        { id: '1', value: 'React' },
+        { id: '2', value: 'Angular' },
+        { id: '3', value: 'Vue' },
+        { id: '4', value: 'React' },
+        { id: '5', value: 'React Native' },
+        { id: '6', value: 'Next' },
+        { id: '7', value: 'Nest' }
+    ],
+    onSelect(item) {
+        console.log(item);
+    }
+});
